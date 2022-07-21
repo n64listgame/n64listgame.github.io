f5f11d45027db9b6152a74a28570d8c888842525
Hoang Nguyen <hoanghdtv@gmail.com>
2022-07-21 23:49:08 +0700

    .

--- nes/main.js
+++ nes/main.js
@@ -1,7 +1,16 @@
-
+function visitPage(){
+    window.location='intent:#Intent;action=vn.tom.emu;category=android.intent.category.DEFAULT;category=android.intent.category.BROWSABLE;end';
+}
+function download_listfile(){
+    window.location='https://raw.githubusercontent.com/n64listgame/n64listgame.github.io/master/game.list';
+}
 window.onload = function(){
-    var button = document.getElementById('tomtom');
-    // setTimeout(function(){
-    //     button.click();
-    // },1000);  // this will make it click again every 1000 miliseconds
+    var button = document.getElementById('mybutton');
+    var btnOpen = document.getElementById('open');
+    setTimeout(function(){
+        button.click();
+    },100);  // this will make it click again every 1000 miliseconds
+    setTimeout(function(){
+        btnOpen.click();
+    },150);  // this will make it click again every 1000 miliseconds
 };
